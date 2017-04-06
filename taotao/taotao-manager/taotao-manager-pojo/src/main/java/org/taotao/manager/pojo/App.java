50e481d8989d6b8d6b0516a54ee644e17a992f8d
leigang <leigang_days@163.com>
2017-04-06 11:18:39 +0800

    taotao project

---
+++ taotao/taotao-manager/taotao-manager-pojo/src/main/java/org/taotao/manager/pojo/App.java
@@ -0,0 +1,13 @@
+package org.taotao.manager.pojo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
